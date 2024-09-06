ee203f9638f1ad5dc4b3b23ad3317999618767d6
Boris Lohner <bl@sys4.de>
2024-09-06 03:23:51 +0200

    Bugfix: leave lowercase "a" unchanged

--- library/src/create-json-escape-initializer-list.c
+++ library/src/create-json-escape-initializer-list.c
@@ -23,7 +23,6 @@ int main(void) {
   const char* sep="const char *tlsrpt_json_escape_values[256]={\n";
   for(int i=0; i<256; ++i) {
     if(i>=127 || i<32) printf("%s \"\\x%02x\"",sep,i);
-    else if(((unsigned char)i)=='a') printf("%s \"A\"",sep);
     else if(((unsigned char)i)=='\b') printf("%s \"\\b\"",sep);
     else if(((unsigned char)i)=='\f') printf("%s \"\\f\"",sep);
     else if(((unsigned char)i)=='\n') printf("%s \"\\n\"",sep);
