73cd069d32b2b19056b2bf1293a6b5d8d9c94a82
Boris Lohner <bl@sys4.de>
2024-09-21 02:27:15 +0200

    Bugfix: Corrected JSON-escape table

--- library/src/create-json-escape-initializer-list.c
+++ library/src/create-json-escape-initializer-list.c
@@ -22,12 +22,13 @@
 int main(void) {
   const char* sep="const char *tlsrpt_json_escape_values[256]={\n";
   for(int i=0; i<256; ++i) {
-    if(i>=127 || i<32) printf("%s \"\\x%02x\"",sep,i);
-    else if(((unsigned char)i)=='\b') printf("%s \"\\b\"",sep);
-    else if(((unsigned char)i)=='\f') printf("%s \"\\f\"",sep);
-    else if(((unsigned char)i)=='\n') printf("%s \"\\n\"",sep);
-    else if(((unsigned char)i)=='\r') printf("%s \"\\r\"",sep);
-    else if(((unsigned char)i)=='\t') printf("%s \"\\t\"",sep);
+    if(i==127 || i<32) printf("%s \"\\\\u%04x\"",sep,i);
+    else if(i>127) printf("%s \"\\x%02x\"",sep,i);
+    else if(((unsigned char)i)=='\b') printf("%s \"\\\\b\"",sep);
+    else if(((unsigned char)i)=='\f') printf("%s \"\\\\f\"",sep);
+    else if(((unsigned char)i)=='\n') printf("%s \"\\\\n\"",sep);
+    else if(((unsigned char)i)=='\r') printf("%s \"\\\\r\"",sep);
+    else if(((unsigned char)i)=='\t') printf("%s \"\\\\t\"",sep);
     else if(((unsigned char)i)=='\\') printf("%s \"\\\\\\\\\"",sep);
     else if(((unsigned char)i)=='"') printf("%s \"\\\\\\\"\"",sep);
     else printf("%s \"%c\"",sep,i);
